8e954d90da88064538a4fd9ac780271c6f1890f3
ying.andrews <ying.andrews@gmail.com>
2023-11-11 11:44:52 -0500

    initial commit.

---
+++ src/main/java/com/bae/careers/BaeCareersApplication.java
@@ -0,0 +1,13 @@
+package com.bae.careers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaeCareersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaeCareersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bae/careers/BaeCareersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bae.careers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaeCareersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
