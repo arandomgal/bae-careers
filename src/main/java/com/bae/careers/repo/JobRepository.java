575c8e121dc67ad8ac3b3c55a1ea73854aeca398
ying.andrews <ying.andrews@gmail.com>
2023-11-11 14:29:50 -0500

    Added more endpoints for search by location and by level.

--- src/main/java/com/bae/careers/repo/JobRepository.java
+++ src/main/java/com/bae/careers/repo/JobRepository.java
@@ -1,10 +1,14 @@
 package com.bae.careers.repo;
 
 import com.bae.careers.domain.Job;
+import com.bae.careers.domain.Level;
+import com.bae.careers.domain.Location;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface JobRepository extends CrudRepository<Job, Integer> {
     List<Job> findJobsByCode(String code);
+    List<Job> findJobsByLocation(Location location);
+    List<Job> findJobsByLevel(Level level);
 }
