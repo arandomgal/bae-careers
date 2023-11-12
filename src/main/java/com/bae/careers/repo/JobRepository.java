f4e8fefd86d5873e33f31de73cbffd1a42133125
ying.andrews <ying.andrews@gmail.com>
2023-11-12 06:50:06 -0500

    Added endpoint for search by mode.

--- src/main/java/com/bae/careers/repo/JobRepository.java
+++ src/main/java/com/bae/careers/repo/JobRepository.java
@@ -3,6 +3,7 @@ package com.bae.careers.repo;
 import com.bae.careers.domain.Job;
 import com.bae.careers.domain.Level;
 import com.bae.careers.domain.Location;
+import com.bae.careers.domain.Mode;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -11,4 +12,5 @@ public interface JobRepository extends CrudRepository<Job, Integer> {
     List<Job> findJobsByCode(String code);
     List<Job> findJobsByLocation(Location location);
     List<Job> findJobsByLevel(Level level);
+    List<Job> findJobsByMode(Mode mode);
 }
