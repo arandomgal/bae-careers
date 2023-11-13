50437bc9defaa8a51f77aac7eb3ffe37d08067af
ying.andrews <ying.andrews@gmail.com>
2023-11-13 09:57:35 -0500

    Added a shell script to build and push image to docker hub.

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+tag="1.0.4"
+echo "Build new artifact for BAE Careers REST API release:$tag"
+echo "==> Step 1: Build Jar file."
+mvn clean install -DskipTests
+echo "==> Step 2: Create new image file."
+docker build . -t ymandrews/bae-careers-rest-api:$tag
+echo "==> Step 3: Push image to docker hub."
+docker push ymandrews/bae-careers-rest-api:$tag
+echo "Done!"
