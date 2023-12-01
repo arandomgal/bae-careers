15e6a41450136d03d5fb8d6d6efc4aa3a1f80b1c
ying.andrews <ying.andrews@gmail.com>
2023-12-01 11:13:44 -0500

    Allow CORS

--- src/main/java/com/bae/careers/repo/JobRepository.java
+++ src/main/java/com/bae/careers/repo/JobRepository.java
@@ -5,9 +5,11 @@ import com.bae.careers.domain.Level;
 import com.bae.careers.domain.Location;
 import com.bae.careers.domain.Mode;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 
+@CrossOrigin
 public interface JobRepository extends CrudRepository<Job, Integer> {
     List<Job> findJobsByCode(String code);
     List<Job> findJobsByLocation(Location location);
